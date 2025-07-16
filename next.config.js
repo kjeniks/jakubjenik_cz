babdb8dc0018159dafb1f7f21370f9c7c735dc51
kjeniks <kjeniks@gmail.com>
2025-07-16 20:14:13 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // důležité pro static export
+  output: 'export', // povolí statický export do složky "out"
 };
 
 module.exports = nextConfig;
+// Tento soubor konfiguruje Next.js pro statický export aplikace.
+// Všechny stránky budou exportovány jako statické HTML soubory do složky "out" při spuštění příkazu `next export`.
